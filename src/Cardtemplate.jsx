612a51f8842a3470273f4f7d38273b649c562de7
Kseth16 <95711296+Kseth16@users.noreply.github.com>
2024-05-06 14:25:18 +0530

    "Convert functional component to class component in Cardtemplate.jsx; log props in constructor"
    Explanation:
    
    The code diff shows the conversion of a functional component to a class component in the file `Cardtemplate.jsx

--- src/Cardtemplate.jsx
+++ src/Cardtemplate.jsx
@@ -3,7 +3,55 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
 
+
+/*
+
+data for cards 
+Job title
+Company name
+Location
+Job description (limited to a certain number of characters with an option to expand)
+Experience required
+Apply button/link
+
+
+*/
+
 function Cardtemplate(){
+
+    const jobCardData=[{
+        jobTitle: 'frontend engineer',
+        companyName: 'weekday',
+        location: 'delhi',
+        jobDesription: 'lorem ipsum..  ',
+        experienceRequired: '1 year',
+
+
+    },
+{
+    jobTitle: 'frontend engineer',
+        companyName: 'weekday',
+        location: 'delhi',
+        jobDesription: 'lorem ipsum..  ',
+        experienceRequired: '1 year',
+},
+{
+    jobTitle: 'frontend engineer',
+    companyName: 'weekday',
+    location: 'delhi',
+    jobDesription: 'lorem ipsum..  ',
+    experienceRequired: '1 year',
+
+
+},
+{
+jobTitle: 'frontend engineer',
+    companyName: 'weekday',
+    location: 'delhi',
+    jobDesription: 'lorem ipsum..  ',
+    experienceRequired: '1 year',
+}]
+
     return(
         <div>
         <p>card template boiler plate</p>
