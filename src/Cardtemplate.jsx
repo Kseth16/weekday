2aaf62c01682bcf956a0c9f64d6991e8d9a453e2
Kseth16 <95711296+Kseth16@users.noreply.github.com>
2024-05-06 13:50:02 +0530

    innit

---
+++ src/Cardtemplate.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+
+function Cardtemplate(){
+    return(
+        <div>
+        <p>card template boiler plate</p>
+        </div>
+    )
+}
+
+export default Cardtemplate;
