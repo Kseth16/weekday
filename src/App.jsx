171a6da5942945953658ad040c2b4f64303a9aed
Kseth16 <95711296+Kseth16@users.noreply.github.com>
2024-05-06 16:19:18 +0530

    changes to get the three in a row to work correctly

--- src/App.jsx
+++ src/App.jsx
@@ -38,48 +38,59 @@ function App() {
     location: 'delhi',
     jobDesription: 'lorem ipsum..  ',
     experienceRequired: '1 year',
+},
+{
+  jobTitle: 'frontend engineer',
+      companyName: 'weekday',
+      location: 'delhi',
+      jobDesription: 'lorem ipsum..  ',
+      experienceRequired: '1 year',
+},
+{
+  jobTitle: 'frontend engineer',
+  companyName: 'weekday',
+  location: 'delhi',
+  jobDesription: 'lorem ipsum..  ',
+  experienceRequired: '1 year',
+
+
+},
+{
+  jobTitle: 'frontend engineer',
+  companyName: 'weekday',
+  location: 'delhi',
+  jobDesription: 'lorem ipsum..  ',
+  experienceRequired: '1 year',
 }]
 
 
 
 
   return (
-    <div className='flex-container'>
-      
-      <Cardtemplate 
-        companyName="Weekday"
-        jobTitle="Frontend Engineer"
-        location="Delhi"
-        jobDesription='lorem ipsum..' 
-        experienceRequired='1 year'
-      />
+<>
+    <div className='maincontainer'>
+      <div className='flex-container'>
 
-<Cardtemplate className='flex-item' 
-        companyName="Weekday"
-        jobTitle="Frontend Engineer"
-        location="Delhi"
-        jobDesription='lorem ipsum..' 
-        experienceRequired='1 year'
-      />
+      {jobCardData.map((job,index)=>(
+        <Cardtemplate
+        key={index}
+        companyName={job.companyName}
+        jobTitle={job.jobTitle}
+        location={job.location}
+        jobDesription={job.jobDesription}
+        experienceRequired={job.experienceRequired}
+        />
+      ))}
 
-<Cardtemplate className='flex-item'
-        companyName="Weekday"
-        jobTitle="Frontend Engineer"
-        location="Delhi"
-        jobDesription='lorem ipsum..' 
-        experienceRequired='1 year'
-      />
+      </div>
 
-<Cardtemplate className='flex-item'
-        companyName="Weekday"
-        jobTitle="Frontend Engineer"
-        location="Delhi"
-        jobDesription='lorem ipsum..' 
-        experienceRequired='1 year'
-      />
 
 
     </div>
+
+
+
+</>
   )
 }
 
--- src/Cardtemplate.jsx
+++ src/Cardtemplate.jsx
@@ -4,20 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 
-// this is the template for all cards
-
-/*
-
-data for cards 
-Job title
-Company name
-Location
-Job description (limited to a certain number of characters with an option to expand)
-Experience required
-Apply button/link
-
-
-*/
 
 export default class Cardtemplate extends React.Component{
 constructor(props){
@@ -35,7 +21,7 @@ constructor(props){
     return(
         
         <div id="cardtemplate">
-<Card variant="outlined" style={{ paddingBottom: "10px" }}>
+    <Card variant="outlined">
     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {this.props.companyName}
     </Typography>
