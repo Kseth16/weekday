ac2ce790d6698c29fbf1450b238803f946fe575f
Kseth16 <95711296+Kseth16@users.noreply.github.com>
2024-05-06 19:32:58 +0530

    updating to allow for automatic fetching of data when page loads

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Cardtemplate from './Cardtemplate'
+//declaring state variable 
+
+
+// "jobDetailsFromCompany": "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
+// "maxJdSalary": 103,
+// "minJdSalary": 100,
+// "salaryCurrencyCode": "USD",
+// "location": "mumbai",
+// "minExp": null,
+// "maxExp": null,
+// "jobRole": "ios",
+// "companyName": "LG",
+// "logoUrl": "https://logo.clearbit.com/lg.com"
 
 function App() {
-  // const [count, setCount] = useState(0)
+let [jobCardData, setJobCardData]= useState([]);
 
 
-  let data;
-
-   let jobCardData=[{
-        jobTitle: 'frontend engineer',
-        companyName: 'weekday',
-        location: 'delhi',
-        jobDesription: 'lorem ipsum..  ',
-        experienceRequired: '1 year',
-
-
-    },
-{
-    jobTitle: 'frontend engineer',
-        companyName: 'weekday',
-        location: 'delhi',
-        jobDesription: 'lorem ipsum..  ',
-        experienceRequired: '1 year',
-},
-{
-    jobTitle: 'frontend engineer',
-    companyName: 'weekday',
-    location: 'delhi',
-    jobDesription: 'lorem ipsum..  ',
-    experienceRequired: '1 year',
-
-
-},
-{
-    jobTitle: 'frontend engineer',
-    companyName: 'weekday',
-    location: 'delhi',
-    jobDesription: 'lorem ipsum..  ',
-    experienceRequired: '1 year',
-},
-{
-  jobTitle: 'frontend engineer',
-      companyName: 'weekday',
-      location: 'delhi',
-      jobDesription: 'lorem ipsum..  ',
-      experienceRequired: '1 year',
-},
-{
-  jobTitle: 'frontend engineer',
-  companyName: 'weekday',
-  location: 'delhi',
-  jobDesription: 'lorem ipsum..  ',
-  experienceRequired: '1 year',
-
-
-},
-{
-  jobTitle: 'frontend engineer',
-  companyName: 'weekday',
-  location: 'delhi',
-  jobDesription: 'lorem ipsum..  ',
-  experienceRequired: '1 year',
-}]
-
-
-
-const myHeaders= new Headers();
-
+useEffect(() => {
+  const fetchData = async () => {
+    const myHeaders= new Headers();
 myHeaders.append("Content-Type", "application/json");
 
 const body=JSON.stringify({
@@ -77,35 +31,54 @@ const body=JSON.stringify({
   "offset": 0
 });
 
+
 const requestOptions ={
   method: "POST",
   headers: myHeaders,
   body
 };
 
-fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
-.then((response) => response.text())
-.then((result) => {
-  console.log(result);
-  data=JSON.stringify(result);
+try{
 
-})
-.catch((error) => console.error(error));
+const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+
+const data = await response.json();
+
+setJobCardData(data.jdList)
+} catch(error){
+  console.error(error);
+}
+  };
+
+  fetchData();
 
 
-  return (
+
+},[]);
+
+
+
+return (
 <>
     <div className='maincontainer'>
       <div className='flex-container'>
 
-      {jobCardData.map((job,index)=>(
+
+      {/* mapping over json data to dynamically render job cards  */}
+      {jobCardData.map((jobCardData,index)=>(
         <Cardtemplate
         key={index}
-        companyName={job.companyName}
-        jobTitle={job.jobTitle}
-        location={job.location}
-        jobDesription={job.jobDesription}
-        experienceRequired={job.experienceRequired}
+        companyName={jobCardData.companyName}
+        minJdSalary={jobCardData.minJdSalary}
+        maxJdSalary={jobCardData.maxJdSalary}
+        salaryCurrencyCode={jobCardData.salaryCurrencyCode}
+        minExp={jobCardData.minExp}
+        maxExp={jobCardData.maxExp}
+        logoUrl={jobCardData.logoUrl}
+        jobRole={jobCardData.jobRole}
+        location={jobCardData.location}
+        jobDesription={jobCardData.jobDetailsFromCompany}
+        experienceRequired={jobCardData.experienceRequired}
         />
       ))}
 
@@ -118,7 +91,7 @@ fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
 
 
 </>
-  )
+  );
 }
 
 export default App
