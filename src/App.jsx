2165affc53ecccfe2f1b28a2573bc2b01f45a636
Kseth16 <95711296+Kseth16@users.noreply.github.com>
2024-05-06 20:09:35 +0530

    retrival works saving before doing changes to get infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Cardtemplate from './Cardtemplate'
-//declaring state variable 
+
 
 
 // "jobDetailsFromCompany": "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
@@ -20,17 +20,30 @@ import Cardtemplate from './Cardtemplate'
 function App() {
 let [jobCardData, setJobCardData]= useState([]);
 
+//declaring state variable 
 
-useEffect(() => {
+let off=9;
+
+useEffect((off) => {
   const fetchData = async () => {
+
+
+    //setting headers parameters
     const myHeaders= new Headers();
-myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+
+// setting body parameters
 
 const body=JSON.stringify({
-  "limit": 10,
-  "offset": 0
+  "limit": 12,
+  "offset": off
 });
 
+
+
+
+//setting api up
 
 const requestOptions ={
   method: "POST",
@@ -38,19 +51,28 @@ const requestOptions ={
   body
 };
 
+
 try{
 
+  // fetching data
 const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
 
+
+
+//data is retrived into data
 const data = await response.json();
 
+
+
+//filling job card with data.jdList or the json data this will be mapped to dynamically render data
 setJobCardData(data.jdList)
 } catch(error){
   console.error(error);
 }
   };
 
-  fetchData();
+fetchData();
+
 
 
 
